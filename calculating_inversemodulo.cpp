3c41de20abcfee1b9e4c297ea69d5a29b5d2b559
violet360 <46649871+violet360@users.noreply.github.com>
2020-01-13 23:09:38 +0530

    Update calculating_inversemodulo.cpp

--- calculating_inversemodulo.cpp
+++ calculating_inversemodulo.cpp
@@ -3,6 +3,10 @@ using namespace std;
   
 // Function to calculate modular 
 // inverse using D.P 
+// it can be used to find nCr%p , nCr = (n! * moduloInverse(n! , p)%p * moduloInverse((n-r)! , p)%p)%p , so modulo inverse can be found by 
+// by fermat's theorem pow(n, p) = n mod p , pow(n, p-2) = pow(n, -1) mod p => pow(n, p-2) = modInverse(n, p) but ...... there's is another
+// method......
+// modularInverse[i] = modularInverse[p % i] * (p - p/i)%p; ----> https://cp-algorithms.com/algebra/module-inverse.html...
 void modularInverse(int n, int prime) 
 { 
     int dp[n + 1]; 
